be8636e0b6da903223b1be085c8d0897257baafc
Wenyao <38875149+shawenyao@users.noreply.github.com>
2022-07-16 22:26:44 -0700

    Update control_ac.py

--- control_ac.py
+++ control_ac.py
@@ -4,15 +4,17 @@ import os
 from dotenv import load_dotenv
 
 def control_ac(on = False):
-
-    # load credentials
+    # load environment variables
     load_dotenv()
+    email = os.environ.get('email')
+    password = os.environ.get('password')
+    device_id = os.environ.get('device_id')
 
     # new session
     s = requests.Session()
 
     # login
-    payload = {"user":{"email": os.environ.get('email'), "password": os.environ.get('password'), "application": {"app_id": "", "app_secret": ""}}}
+    payload = {"user":{"email": email, "password": password, "application": {"app_id": "", "app_secret": ""}}}
     response_login = s.post('https://connectstat.carrier.com/api/users/sign_in', json=payload)
 
     headers = {
@@ -29,7 +31,7 @@ def control_ac(on = False):
 
     # send request to server
     s.post(
-        'https://connectstat.carrier.com/api-devices-field/apiv1/dsns/' + os.environ.get('device_id') + '/properties/UsrMd1/datapoints',
+        'https://connectstat.carrier.com/api-devices-field/apiv1/dsns/' + device_id + '/properties/UsrMd1/datapoints',
         headers=headers,
         json={"datapoint": {"value": action_value}}
     )
@@ -38,11 +40,9 @@ def control_ac(on = False):
     s.close()
 
 if __name__ == '__main__':
-
     if len(sys.argv) < 2:
         # turn off ac
-        control_ac()
-        
+        control_ac()    
     elif sys.argv[1] == 'on':
         # turn on ac
         control_ac(on=True)
